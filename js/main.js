c1742c7edc4df868792aba5e3d08696d65119a67
Stephanie Lee <stephmylee@Stephanies-MacBook-Pro-7.local>
2019-05-19 17:15:19 -0400

    changed name

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Stephanie Lee.", "Neuroscientist.", "Creator.", "Adventurer.", "Animal Lover"],
       typeSpeed: 100,
       loop: true,
     });
